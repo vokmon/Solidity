8634cb65a80ac85ee44662a4a7a7108787971f2c
Arnon Ruangthanawes <arnon@precisionsolutions.co.nz>
2019-06-03 10:23:31 +0700

    Variable type

--- MyContract.sol
+++ MyContract.sol
@@ -1,17 +1,40 @@
-pragma solidity ^0.4.24;
+pragma solidity 0.5.9;
 
 contract MyContract {
-    string value;
     
-    constructor() public {
-        value = "My Value";
-    }
+    // State variable, set to public
+    // Can be accessed from outside
+    string public value = "My Value";
     
-    function get() public view returns(string) {
-        return value;
-    }
+    // Constant value
+    string public constant constantValue = "Constant value";
     
-    function set(string _value) public {
+    bool public myBool = true;
+    
+    // signed integer: can be negative e.g. -100
+    int public myInt = -10;
+    
+    // Unsigned integer: cannot be negative
+    uint public myUInit = 1;
+    
+    uint8 public myInt8 = 8;
+    uint256 public myInt256 = 999999;
+    
+    // constructor() public {
+    //     value = "My Value";
+    // }
+    
+    
+    function setValue(string memory _value) public {
         value = _value;
+    }
+    
+    // These 2 methods can be omitted as the variables are public
+    function get() public view returns(string memory) {
+      return value;
+    }
+    
+    function getConstant() public pure returns(string memory) {
+        return constantValue;
     }
 }
